6f9e4f5b788e16cb712df6b3bdb75bbb660261dd
titosco <titobiloluwatella@gmail.com>
2024-05-28 22:55:21 +0000

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,61 @@
-#!/usr/bin/python3
-"""This script solves the N-Queens problem"""
 
+#!/usr/bin/python3
+"""Solution to the N-Queens puzzle"""
 import sys
+
+
+def print_board(board, n):
+    """prints allocated possitions to the queen"""
+    b = []
+
+    for i in range(n):
+        for j in range(n):
+            if j == board[i]:
+                b.append([i, j])
+    print(b)
+
+
+def safe_position(board, i, j, r):
+    """Determines whether the position is safe for the queen"""
+    return board[i] in (j, j - i + r, i - r + j)
+
+
+def determine_positions(board, row, n):
+    """Recursively finds all safe positions where the queen can be allocated"""
+    if row == n:
+        print_board(board, n)
+
+    else:
+        for j in range(n):
+            allowed = True
+            for i in range(row):
+                if safe_position(board, i, j, row):
+                    allowed = False
+            if allowed:
+                board[row] = j
+                determine_positions(board, row + 1, n)
+
+
+def create_board(size):
+    """Generates the board"""
+    return [0 * size for i in range(size)]
+
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
 try:
-    N = int(sys.argv[1])
-except ValueError:
+    n = int(sys.argv[1])
+except BaseException:
     print("N must be a number")
     exit(1)
 
-if N < 4:
+if (n < 4):
     print("N must be at least 4")
     exit(1)
 
-columns = set()
-pos_diagonal = set()
-neg_diagonal = set()
-board = []
+board = create_board(int(n))
+row = 0
+determine_positions(board, row, int(n))
 
-# create the board positions
-for _ in range(N):
-    row = [0] * 2
-    board.append(row)
-
-
-def is_safe(r, c):
-    """Check if a queen can be placed at position (r, c)"""
-    if c in columns or (r + c) in pos_diagonal or (r - c) in neg_diagonal:
-        return False
-    return True
-
-
-def solve_n_queens(r):
-    """recursively places N queens on an N x N chessboard"""
-    if r == N:
-        print(board)
-        return
-
-    for c in range(N):
-        if is_safe(r, c):
-            columns.add(c)
-            pos_diagonal.add(r + c)
-            neg_diagonal.add(r - c)
-            board[r] = [r, c]
-            solve_n_queens(r + 1)
-            # Backtrack
-            columns.remove(c)
-            pos_diagonal.remove(r + c)
-            neg_diagonal.remove(r - c)
-            board[r] = [0, 0]
-
-
-solve_n_queens(0)
